28fde14410d80367d4b35db96ef76b62222a20a8
Adam Nagy <adam_e_nagy@hotmail.com>
2015-11-10 02:47:13 +0000

    Code formatting

--- Swift/PicOnPic/ViewController.swift
+++ Swift/PicOnPic/ViewController.swift
@@ -12,131 +12,131 @@ import MobileCoreServices
 class ViewController : UIViewController,
   UIImagePickerControllerDelegate,
   UINavigationControllerDelegate,
-  UIPopoverControllerDelegate {
-
+UIPopoverControllerDelegate {
+  
   var overlayImageView: UIImageView?
   var imageFromCamera = true;
   @IBOutlet weak var imageView: UIImageView!
   
   override func viewDidLoad() {
-		print("viewDidLoad")
-		super.viewDidLoad()
-	}
-	
-	override func didReceiveMemoryWarning() {
-		print("didReceiveMemoryWarning")
-		super.didReceiveMemoryWarning()
-	}
+    print("viewDidLoad")
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    print("didReceiveMemoryWarning")
+    super.didReceiveMemoryWarning()
+  }
   
   @IBAction func startCamera(sender: UIBarButtonItem) {
-		print("startCamera")
-
-		if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-			let imagePicker: UIImagePickerController = UIImagePickerController()
-			imagePicker.delegate = self
-			imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-			imagePicker.mediaTypes = NSArray(objects: kUTTypeImage) as! [String]
-			imagePicker.allowsEditing = false
-			presentViewController(
+    print("startCamera")
+    
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+      let imagePicker: UIImagePickerController = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+      imagePicker.mediaTypes = NSArray(objects: kUTTypeImage) as! [String]
+      imagePicker.allowsEditing = false
+      presentViewController(
         imagePicker, animated: true, completion: {
           print("startCamera - completion")
           self.imageFromCamera = true
-        })
-		}
+      })
+    }
   }
-
-	@IBAction func findPic(sender: UIBarButtonItem) {
-		print("findPic")
+  
+  @IBAction func findPic(sender: UIBarButtonItem) {
+    print("findPic")
     
-		if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum) {
-			let imagePicker: UIImagePickerController = UIImagePickerController()
-			imagePicker.delegate = self
-			imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-			imagePicker.mediaTypes = NSArray(objects: kUTTypeImage) as! [String]
-			imagePicker.allowsEditing = false
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+      let imagePicker: UIImagePickerController = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+      imagePicker.mediaTypes = NSArray(objects: kUTTypeImage) as! [String]
+      imagePicker.allowsEditing = false
       presentViewController(
         imagePicker, animated: true, completion: {
           print("findPic - completion")
           self.imageFromCamera = false
-        })
-		}
-	}
-	
-	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-		print("imagePickerController")
-		picker.dismissViewControllerAnimated(true, completion: nil)
-		let image: UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
-		// If it's a picture selection or
+      })
+    }
+  }
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    print("imagePickerController")
+    picker.dismissViewControllerAnimated(true, completion: nil)
+    let image: UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+    // If it's a picture selection or
     // one from the camera
     if imageFromCamera {
       imageView.image = image
     } else {
       if (overlayImageView != nil) {
-				overlayImageView!.removeFromSuperview()
-			}
+        overlayImageView!.removeFromSuperview()
+      }
       
-			overlayImageView = UIImageView(image: image)
-			imageView.addSubview(overlayImageView!)
+      overlayImageView = UIImageView(image: image)
+      imageView.addSubview(overlayImageView!)
     }
-	}
-	
-	@IBAction func pinch(sender: UIPinchGestureRecognizer) {
-		print("pinch")
+  }
+  
+  @IBAction func pinch(sender: UIPinchGestureRecognizer) {
+    print("pinch")
     struct funcData {
       static var origTr = CGAffineTransform()
     }
     
-		if sender.state == UIGestureRecognizerState.Began {
-			funcData.origTr = overlayImageView!.transform
-		} else {
-			if sender.state == UIGestureRecognizerState.Changed {
-				let scale: CGFloat = sender.scale
-				let tr: CGAffineTransform =
-          CGAffineTransformConcat(funcData.origTr,CGAffineTransformMakeScale(scale,scale))
-				overlayImageView!.transform = tr
-			}
-		}
-	}
-	
-	@IBAction func pan(sender: UIPanGestureRecognizer) {
-		print("pan")
+    if sender.state == UIGestureRecognizerState.Began {
+      funcData.origTr = overlayImageView!.transform
+    } else {
+      if sender.state == UIGestureRecognizerState.Changed {
+        let scale: CGFloat = sender.scale
+        let tr: CGAffineTransform =
+        CGAffineTransformConcat(funcData.origTr,CGAffineTransformMakeScale(scale,scale))
+        overlayImageView!.transform = tr
+      }
+    }
+  }
+  
+  @IBAction func pan(sender: UIPanGestureRecognizer) {
+    print("pan")
     struct funcData {
       static var prevPt = CGPoint()
     }
     
-		if sender.state == UIGestureRecognizerState.Began {
+    if sender.state == UIGestureRecognizerState.Began {
       print("pan - Began")
-			funcData.prevPt = sender.locationInView(imageView)
-		} else if sender.state == UIGestureRecognizerState.Changed {
+      funcData.prevPt = sender.locationInView(imageView)
+    } else if sender.state == UIGestureRecognizerState.Changed {
       print("pan - Changed")
       let pt: CGPoint = sender.locationInView(imageView)
       let tr: CGAffineTransform = CGAffineTransformMakeTranslation(
-          pt.x-funcData.prevPt.x,pt.y-funcData.prevPt.y)
+        pt.x-funcData.prevPt.x,pt.y-funcData.prevPt.y)
       let cp: CGPoint =
-        CGPointApplyAffineTransform(
-          overlayImageView!.center,tr)
+      CGPointApplyAffineTransform(
+        overlayImageView!.center,tr)
       overlayImageView!.center = cp
       funcData.prevPt = pt
-		}
-	}
-	
-	@IBAction func rotate(sender: UIRotationGestureRecognizer) {
-		print("rotate")
+    }
+  }
+  
+  @IBAction func rotate(sender: UIRotationGestureRecognizer) {
+    print("rotate")
     struct funcData {
       static var origTr = CGAffineTransform()
     }
     
-		if sender.state == UIGestureRecognizerState.Began {
-			funcData.origTr = overlayImageView!.transform
-		} else {
-			if sender.state == UIGestureRecognizerState.Changed {
-				let rotation: CGFloat = sender.rotation
-				// Scale
-				let tr: CGAffineTransform =
-          CGAffineTransformConcat(funcData.origTr,CGAffineTransformMakeRotation(rotation))
-				overlayImageView!.transform = tr
-			}
-		}
-	}
+    if sender.state == UIGestureRecognizerState.Began {
+      funcData.origTr = overlayImageView!.transform
+    } else {
+      if sender.state == UIGestureRecognizerState.Changed {
+        let rotation: CGFloat = sender.rotation
+        // Scale
+        let tr: CGAffineTransform =
+        CGAffineTransformConcat(funcData.origTr,CGAffineTransformMakeRotation(rotation))
+        overlayImageView!.transform = tr
+      }
+    }
+  }
 }
 
